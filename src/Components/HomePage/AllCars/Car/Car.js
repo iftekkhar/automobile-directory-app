6fb33385489aad06b2f98b09512e11769c994d7e
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-12-11 05:46:52 +0600

    car detail page & Routing implemented

--- src/Components/HomePage/AllCars/Car/Car.js
+++ src/Components/HomePage/AllCars/Car/Car.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
 
 const Car = () => {
+    const [currentCar, setCurrentCar] = useState({})
+    const cars = useSelector(state => state.cars)
+    let { id } = useParams();
+    useEffect(() => {
+        const filteredCar = cars.filter(car => car._id === id);
+        setCurrentCar(filteredCar[0]);
+    }, [id, cars])
     return (
         <div>
-            <h1>Car Details</h1>
+            <h1>{currentCar?.title}</h1>
+            <p>{currentCar?.model}</p>
+            <p>{currentCar?.details}</p>
+            <img src={currentCar?.featuredImage} />
         </div>
     );
 };
