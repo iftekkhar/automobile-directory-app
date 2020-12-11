d7907a1f902691f819f1b3f01e8ff96db9fbde61
Iftekhar Anam <iftekhar_anam@hotmail.com>
2020-12-11 08:14:29 +0600

    filter functionality added

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
+import { CircularProgress } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import AllCars from './AllCars/AllCars';
 
 const HomePage = () => {
     const cars = useSelector(state => state.cars)
-    console.log(cars)
-    return (
+    const [filteredResult, setFilteredResult] = useState([])
+    const [search, setSearch] = useState("");
+    useEffect(() => {
+        setFilteredResult(
+            cars.filter(car =>
+                car.title.toLowerCase().includes(search.toLowerCase())
+            )
+        );
+    }, [search, cars]);
+
+    return !cars.length ? <CircularProgress /> : (
         <div>
-            {cars.map(car => (
-                <AllCars key={car._id} car={car} />
-            ))}
+            <input
+                type="text"
+                placeholder="Search Vehicles"
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            { filteredResult.length ?
+                filteredResult.map(car => (
+                    <AllCars key={car._id} car={car} />
+                ))
+                :
+                <h2>Nothing Found</h2>
+            }
 
         </div>
     );
